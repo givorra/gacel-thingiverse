6764ab05f4ead4a08cb0e0f28dc85ff9716fdc4e
Gacel <gacel@innovaasv.com>
2020-09-13 02:18:46 +0200

    login component

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import {useQuery} from "@apollo/client";
 import {GQL_AUTHENTICATION_REDIRECT_URI} from "../../graphql/queries";
 import {AthenticationRedirectUrlData} from "./interfaces";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 function Login(): JSX.Element {
     const {loading, error, data} = useQuery<AthenticationRedirectUrlData>(GQL_AUTHENTICATION_REDIRECT_URI);
@@ -12,9 +15,24 @@ function Login(): JSX.Element {
         return (<p>Error [{JSON.stringify(error)}]</p>);
     } else {
         return (
-            <div>
-                <a href={data?.authenticationRedirectUrl}>Login with Thingiverse guy!</a>
-            </div>
+            <Container>
+                <Row>
+                    <div className="mx-auto pt-5">
+                        <h1 className="text-center">¡Welcome to Gacel's Thingiverse!</h1>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="mx-auto pt-5">
+                        <p className="text-center">Please, login to Thingivierse to continue</p>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="mx-auto pt-2">
+                        <a href={data?.authenticationRedirectUrl}><Button className="text-center">Login with Thingiverse</Button></a>
+                    </div>
+                </Row>
+            </Container>
+
         );
     }
 }
